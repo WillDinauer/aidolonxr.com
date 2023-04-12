2be4a6ce763be7c73364b59eb5e51f5279f51dc1
Will Dinauer <77174175+WillDinauer@users.noreply.github.com>
2023-04-12 14:47:16 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $('form').submit(function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    
+    // Get the form data
+    var formData = $(this).serialize();
+    
+    // Submit the form data using AJAX
+    $.ajax({
+      type: 'POST',
+      url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfCDfL1nBoGPHRswOgUQ4WboeqAcs14G-kWAsbL5TpSqT_oaA/formResponse",
+      data: formData,
+      dataType: 'xml',
+      success: function() {
+        // Show a success message
+        alert('Form submitted successfully!');
+        
+        // Clear the form
+        $('form').trigger('reset');
+      },
+      error: function() {
+        // Show an error message
+        alert('Form submission failed!');
+        
+        // Clear the form
+        $('form').trigger('reset');
+      }
+    });
+  });
+});
